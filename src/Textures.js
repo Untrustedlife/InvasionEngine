71058c8200406bd10cf6066b4193c76eeac5a628
rose <rose@Wild.roses.space>
2025-08-29 22:01:37 -0500

    replaced all wall textures

--- src/Textures.js
+++ src/Textures.js
@@ -346,8 +346,12 @@ export async function initAsyncTextures() {
   try {
     let imagesToReplace = [
       { index: 1, image: "OfficeWall.png" },
+      { index: 2, image: "Panel.png" },
+      { index: 3, image: "Hedge.png" },
       { index: 4, image: "OfficeDoor.png" },
+      { index: 5, image: "Portal.png" },
       { index: 6, image: "OfficeDoorBlue.png" },
+      { index: 7, image: "Field.png" },
       // Add more textures to replace if needed
     ];
     await loadImagesToReplaceTextures(imagesToReplace);
